90ef7f2249586029774c938e71d081a8e90fea95
Meg Onyango <onyangomeg@gmail.com>
2022-02-23 15:54:56 +0300

    Update handle_glogin.php

--- chromiumproject/handle_glogin.php
+++ chromiumproject/handle_glogin.php
@@ -6,10 +6,10 @@ include "google-api/vendor/autoload.php";
 $gClient = new Google_Client();
 
 
-$gClient->setClientId("913884675007-eibhucichskt8t9fiuflftsn944erj15.apps.googleusercontent.com");
-$gClient->setClientSecret("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");
+$gClient->setClientId("your id");
+$gClient->setClientSecret("your secret");
 $gClient->setApplicationName("Chromium Login");
-$gClient->setRedirectUri("http://localhost/WebdevClasses2022/chromiumproject/dashboard.php");
+$gClient->setRedirectUri("your redirect");
 $gClient ->addScope("https://www.googleapis.com/auth/plus.login  https://www.googleapis.com/auth/userinfo.email");
 
 
